9c13dc9b90a96a2d7fae8f6768453ec90a197fa6
Igor Santos Mascarenhas <igorsmascarenhas@outlook.com>
2021-07-02 15:30:27 -0300

    add enums

--- src/main/java/com/mercadolibre/dambetan01/model/Product.java
+++ src/main/java/com/mercadolibre/dambetan01/model/Product.java
@@ -1,5 +1,6 @@
 package com.mercadolibre.dambetan01.model;
 
+import com.mercadolibre.dambetan01.model.enums.ProductType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,7 +19,7 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productId;
-    private String type;
+    private ProductType type;
 
     @ManyToOne
     @JoinColumn(name = "fk_seller", referencedColumnName = "cnpj")
