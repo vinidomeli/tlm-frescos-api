f4d93bdb3d1e2192a3450aa5fde681d92020bc91
Vinicius Lima <vinicius.limafernandes@mercadolivre.com>
2021-07-02 12:51:24 -0300

    [REQ-01] Imports fix

--- src/main/java/com/mercadolibre/dambetan01/service/crud/BatchService.java
+++ src/main/java/com/mercadolibre/dambetan01/service/crud/BatchService.java
@@ -1,5 +1,8 @@
 package com.mercadolibre.dambetan01.service.crud;
 
+import com.mercadolibre.dambetan01.dtos.request.InboundOrderRequestDTO;
+import com.mercadolibre.dambetan01.dtos.response.BatchStockResponseDTO;
+
 public interface BatchService {
 
     public BatchStockResponseDTO registerNewBatch(InboundOrderRequestDTO inboundOrderRequestDTO);
