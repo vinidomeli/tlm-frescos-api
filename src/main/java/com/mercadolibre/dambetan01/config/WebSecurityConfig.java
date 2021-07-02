c1490a9d91e6d53720f7190852970c249d870c94
Gabriel de Andrade Rodrigues <gabriel.deandraderodrigues@mercadolivre.com>
2021-07-02 12:16:49 -0300

    [REQ-01] including migrations with Flyway

--- src/main/java/com/mercadolibre/dambetan01/config/WebSecurityConfig.java
+++ src/main/java/com/mercadolibre/dambetan01/config/WebSecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
+    /*@Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
@@ -22,5 +22,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/fake").permitAll()
 
                 .anyRequest().authenticated();
+    }*/
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/h2-console/**").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
     }
 }
