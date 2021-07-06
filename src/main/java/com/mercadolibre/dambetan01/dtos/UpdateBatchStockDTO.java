7a3892cbe5c891b2b61967cc699c6513ad40baea
Luana Tammy Uchida Ogawa <logawa@br0c02dxa86md6n.home>
2021-07-06 12:26:21 -0300

    [REQ-01] correcting issues in PUT request

--- src/main/java/com/mercadolibre/dambetan01/dtos/UpdateBatchStockDTO.java
+++ src/main/java/com/mercadolibre/dambetan01/dtos/UpdateBatchStockDTO.java
@@ -1,5 +1,13 @@
 package com.mercadolibre.dambetan01.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.*;
 
 import javax.validation.constraints.NotNull;
@@ -15,32 +23,50 @@ import java.time.LocalDateTime;
 @Builder
 public class UpdateBatchStockDTO {
 
+    @JsonProperty("batchNumber")
     @NotNull(message = "Batch number is required.")
     private Long batchNumber;
 
+    @JsonProperty("productId")
     @NotNull(message = "Product ID is required.")
     private Long productId;
 
+    @JsonProperty("currentTemperature")
     @NotNull(message = "Current temperature is required.")
     private Double currentTemperature;
 
+    @JsonProperty("minimumTemperature")
     @NotNull(message = "Minimum temperature is required.")
     private Double minimumTemperature;
 
+    @JsonProperty("initialQuantity")
     @NotNull(message = "Initial quantity is required.")
     @Positive(message = "Initial quantity should be positive.")
     private Integer initialQuantity;
 
+    @JsonProperty("currentQuantity")
     @NotNull(message = "Current quantity is required.")
     @PositiveOrZero(message = "Current quantity should be positive.")
     private Integer currentQuantity;
 
+    @JsonProperty("manufacturingDate")
     @NotNull(message = "Manufacturing date is required.")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate manufacturingDate;
 
+    @JsonProperty("manufacturingTime")
     @NotNull(message = "Manufacturing time is required.")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime manufacturingTime;
 
+    @JsonProperty("dueDate")
     @NotNull(message = "Due date is required.")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate dueDate;
 }
