b7a517110c94086161ef98b099560a55a9cd2eaa
Igor Santos Mascarenhas <igorsmascarenhas@outlook.com>
2021-07-06 09:51:28 -0300

    C[REQ-01] First commit. Add responseDTO

---
+++ src/main/java/com/mercadolibre/dambetan01/dtos/SectionDTO.java
@@ -0,0 +1,12 @@
+package com.mercadolibre.dambetan01.dtos;
+
+import com.mercadolibre.dambetan01.model.Warehouse;
+
+import java.util.UUID;
+
+public class SectionDTO {
+
+    private UUID sectionCode;
+    private UUID warehouseCode;
+
+}
