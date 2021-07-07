efcbc08cb4c07da4611b5706a78aba1c6ddd38ef
Igor Santos Mascarenhas <igorsmascarenhas@outlook.com>
2021-07-07 14:55:12 -0300

    add feature requirements

--- src/main/java/com/mercadolibre/dambetan01/service/crud/impl/BatchServiceImpl.java
+++ src/main/java/com/mercadolibre/dambetan01/service/crud/impl/BatchServiceImpl.java
@@ -2,6 +2,7 @@ package com.mercadolibre.dambetan01.service.crud.impl;
 
 import com.mercadolibre.dambetan01.dtos.BatchStockDTO;
 import com.mercadolibre.dambetan01.exceptions.ApiException;
+import com.mercadolibre.dambetan01.exceptions.NotFoundException;
 import com.mercadolibre.dambetan01.model.Batch;
 import com.mercadolibre.dambetan01.model.InboundOrder;
 import com.mercadolibre.dambetan01.model.Product;
@@ -11,7 +12,9 @@ import com.mercadolibre.dambetan01.repository.ProductRepository;
 import com.mercadolibre.dambetan01.service.crud.BatchService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BatchServiceImpl implements BatchService {
@@ -25,6 +28,43 @@ public class BatchServiceImpl implements BatchService {
         this.productRepository = productRepository;
         this.inboundOrderRepository = inboundOrderRepository;
         this.batchRepository = batchRepository;
+    }
+
+    public List<Batch> findBatchesByProductId(Long productId) {
+        Optional<Product> productOptional = productRepository.findById(productId);
+
+        if(!productOptional.isPresent()) {
+            throw new NotFoundException("Produto com id" + productId +  "não encontrado!");
+        }
+
+        return batchRepository.findBatchesByProduct_Id(productId);
+    }
+
+    public List<Batch> findBatchesByProductId(List<String> query) throws NotFoundException {
+        Long productId = Long.parseLong(query.get(0));
+        Optional<Product> productOptional = productRepository.findById(productId);
+        LocalDate validDueDate = LocalDate.now().plusWeeks(3);
+
+
+        if(!productOptional.isPresent()) {
+            throw new NotFoundException("");
+        }
+
+        List<Batch> batches = null;
+
+        if(query.size() == 2) {
+            if(query.get(1).equals("C")) {
+                batches = batchRepository.findBatchesByProduct_IdAndDueDateGreaterThanOrderByCurrentQuantity(productId, validDueDate);
+            } else if(query.get(1).equals("F")) {
+                batches = batchRepository.findBatchesByProduct_IdAndDueDateGreaterThanOrderByDueDateAsc(productId, validDueDate);
+            }
+        }
+
+        if(batches == null) {
+            batches = batchRepository.findBatchesByProduct_IdAndDueDateGreaterThanOrderByDueDateAsc(productId, validDueDate);
+        }
+
+        return batches;
     }
 
     @Override
@@ -70,5 +110,20 @@ public class BatchServiceImpl implements BatchService {
                 throw new ApiException("404", "Batch number " + batchNumber + " doesn't exists", 404);
             }
         });
+    }
+
+    @Override
+    public Batch createBatch(Product product, InboundOrder inboundOrder) {
+
+        Batch batch = new Batch();
+        batch.setProduct(product);
+        batch.setCurrentQuantity(1);
+        batch.setDueDate(LocalDate.now());
+        batch.setCurrentTemperature(1.0);
+        batch.setInboundOrder(inboundOrder);
+        batch.setInitialQuantity(1);
+        batch.setCurrentQuantity(1);
+
+        return batchRepository.save(batch);
     }
 }
