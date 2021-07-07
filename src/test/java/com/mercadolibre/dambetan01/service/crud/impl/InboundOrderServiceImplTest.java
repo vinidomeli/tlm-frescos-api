8dc50b75eb8c3fb2c68fb81c96e8502a519350c2
Luana Tammy Uchida Ogawa <logawa@br0c02dxa86md6n.home>
2021-07-06 22:01:12 -0300

    [REQ-01] adding Batch and InboundOrder Services Tests

---
+++ src/test/java/com/mercadolibre/dambetan01/service/crud/impl/InboundOrderServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.mercadolibre.dambetan01.service.crud.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InboundOrderServiceImplTest {
+
+    @Test
+    void registerNewInboundOrder() {
+    }
+
+    @Test
+    void updateInboundOrder() {
+    }
+
+    @Test
+    void orderNumberExists() {
+    }
+
+    @Test
+    void inboundOrderContainsSectionCode() {
+    }
+
+    @Test
+    void inboundOrderContainsBatchNumbers() {
+    }
+}
