40878afdd2f7fdfbce7d2ab7372a102f9116fdd9
Luana Tammy Uchida Ogawa <logawa@br0c02dxa86md6n.ml.com>
2021-07-07 15:30:30 -0300

    [REQ-05] adding response DTOs

--- src/main/java/com/mercadolibre/dambetan01/dtos/response/BatchStockDueDateDTO.java
+++ src/main/java/com/mercadolibre/dambetan01/dtos/response/BatchStockDueDateDTO.java
@@ -1,4 +1,15 @@
 package com.mercadolibre.dambetan01.dtos.response;
 
+import com.mercadolibre.dambetan01.dtos.BatchDueDateDTO;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BatchStockDueDateDTO {
+    List<BatchDueDateDTO> batchStockDueDate;
 }
