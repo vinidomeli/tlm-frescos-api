cd570ddc8e3330e004cc4b8fab736159ba693507
Igor Santos Mascarenhas <igorsmascarenhas@outlook.com>
2021-07-07 15:09:04 -0300

    [REQ-01] First commit. Add requirements

--- src/main/java/com/mercadolibre/dambetan01/service/crud/BatchService.java
+++ src/main/java/com/mercadolibre/dambetan01/service/crud/BatchService.java
@@ -2,11 +2,15 @@ package com.mercadolibre.dambetan01.service.crud;
 
 import com.mercadolibre.dambetan01.dtos.BatchStockDTO;
 import com.mercadolibre.dambetan01.model.Batch;
+import com.mercadolibre.dambetan01.model.InboundOrder;
+import com.mercadolibre.dambetan01.model.Product;
 
 import java.util.List;
 
 public interface BatchService {
 
-    public Batch convertBatchStockDTOToBatch(BatchStockDTO batchStockDTO, Long inboundOrder);
-    public void batchNumbersExist(List<Long> batchNumbers);
+    Batch convertBatchStockDTOToBatch(BatchStockDTO batchStockDTO, Long inboundOrder);
+    void batchNumbersExist(List<Long> batchNumbers);
+    Batch createBatch(Product product, InboundOrder inboundOrder);
+
 }
