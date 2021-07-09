1f52bbcb42e38b18bffccca033ed7206fc750976
Luana Tammy Uchida Ogawa <logawa@br0c02dxa86md6n.home>
2021-07-09 09:54:54 -0300

    [REQ-05] adding unit tests

--- src/main/java/com/mercadolibre/dambetan01/dtos/response/BatchStockDueDateDTO.java
+++ src/main/java/com/mercadolibre/dambetan01/dtos/response/BatchStockDueDateDTO.java
@@ -4,6 +4,7 @@ import com.mercadolibre.dambetan01.dtos.BatchDueDateDTO;
 import lombok.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -12,4 +13,17 @@ import java.util.List;
 @NoArgsConstructor
 public class BatchStockDueDateDTO {
     List<BatchDueDateDTO> batchStockDueDate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BatchStockDueDateDTO that = (BatchStockDueDateDTO) o;
+        return Objects.equals(batchStockDueDate, that.batchStockDueDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(batchStockDueDate);
+    }
 }
