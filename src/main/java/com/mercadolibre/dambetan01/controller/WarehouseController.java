3d930aedbf981184040ced9ebe9b08f3d02904a8
Igor Santos Mascarenhas <igorsmascarenhas@outlook.com>
2021-07-12 12:37:03 -0300

    Refactor warehouse controller and add swagger

--- src/main/java/com/mercadolibre/dambetan01/controller/WarehouseController.java
+++ src/main/java/com/mercadolibre/dambetan01/controller/WarehouseController.java
@@ -6,6 +6,8 @@ import com.mercadolibre.dambetan01.dtos.response.ProductInWarehousesDTO;
 import com.mercadolibre.dambetan01.dtos.response.WarehouseResponseDTO;
 import com.mercadolibre.dambetan01.service.crud.BatchService;
 import com.mercadolibre.dambetan01.service.crud.WarehouseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/warehouse")
+@Tag(name = "Warehouse Operations")
 public class WarehouseController {
 
     final WarehouseService warehouseService;
@@ -25,6 +28,7 @@ public class WarehouseController {
         this.batchService = batchService;
     }
 
+    @Operation(summary = "Get all warehouses", description = "List all warehouses")
     @GetMapping("/list")
     public ResponseEntity<List<WarehouseDTO>> findAllWarehouses() {
         List<WarehouseDTO> response = this.warehouseService.findAll();
@@ -32,12 +36,14 @@ public class WarehouseController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @Operation(summary = "Create warehouse", description = "Create a warehouse")
     @PostMapping("/create")
     public ResponseEntity<WarehouseResponseDTO> registerWarehouse(@RequestBody @Valid WarehouseRequestDTO warehouseRequestDTO) {
         WarehouseResponseDTO response = this.warehouseService.registerWarehouse(warehouseRequestDTO);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Find product in warehouse", description = "Find product in warehouse by product id")
     @GetMapping("/")
     public ResponseEntity<ProductInWarehousesDTO> findProductInWarehouses(@RequestParam Long productId) {
         ProductInWarehousesDTO response = this.batchService.findProductInWarehousesBy(productId);
